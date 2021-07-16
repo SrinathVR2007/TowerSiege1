1401846111e5018e7dfb45d67fc39050bb4dbd92
SrinathVR2007 <82932228+SrinathVR2007@users.noreply.github.com>
2021-07-16 16:46:57 +0530

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,25 @@
+//class Polygon{
+    //constructor(x,y){
+       // var options ={
+
+       // }
+        //this.body = Bodies.rectangle(x, y, width, height, options);
+        //this.width = 10;
+        //this.height = 10;
+        //this.x = x;
+        //this.y=y;
+        //orld.add(world, this.body);
+        //this.image = loadImage("polygon.png");
+  //  }
+    
+    //display(){
+      //  var angle = this.body.angle;
+     //   var pos= this.body.position;
+    //    push();
+    //    translate(pos.x, pos.y);
+    //    rotate(angle);
+     //   rectMode(CENTER);
+    //    image(this.image,pos.x,pos.y,50, 50);
+     //   pop();
+     // }
+//}
